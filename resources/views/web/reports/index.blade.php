5c3c72c7d0bd92111948268517b98902dfa31622
abarreraaponte <alejandro@dinamo24.com>
2019-04-04 16:06:29 -0400

    I'm Awesome, Sales Report Complete

--- resources/views/web/reports/index.blade.php
+++ resources/views/web/reports/index.blade.php
@@ -23,9 +23,9 @@
 				            </thead>
 				            <tbody>
                                 <tr>
-                                    <td>Reporte de Clientes</td>
+                                    <td>Reporte de Ventas</td>
                                     <td>
-                                        <button type="button" class="btn btn-sm btn-primary" href="#" data-toggle="modal" data-target="#customerreport"><i class="fas fa-eye"></i></button>
+                                        <button type="button" class="btn btn-sm btn-primary" href="#" data-toggle="modal" data-target="#salesreport"><i class="fas fa-search"></i></button>
                                     </td>
                                 </tr>
 				            </tbody>
@@ -38,40 +38,52 @@
     </div>
 </div>
 
-<!--Customer Report Modal -->
-<div class="modal fade" id="customerreport" role="dialog" tabindex="-1" aria-labelledby="customerreport" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <a class="h5 modal-title" id="contactmodallabel">Agregar Filtros</a>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form method="POST" action="{{ '/web/reports/customers' }}">
-                        @csrf
-                        <label class="mt-3">Solo Clientes Activos? <span class="text-muted">Opcional</span></label>
-                        <br>
-                        <input type="checkbox" checked name="active" value="1">
+<!--Sales Report Modal -->
+<div class="modal fade" id="salesreport" role="dialog" tabindex="-1" aria-labelledby="salesreport" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header bg-success text-white">
+                <a class="h5 modal-title" id="contactmodallabel">Filtros - Reporte de Ventas</a>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form method="POST" action="{{ '/web/reports/sales' }}">
+                    @csrf
 
-                        <br>
+                    <label class="mt-3">Fecha Desde <span class="text-muted">Opcional</span></label>
+                    <input type="date" class="form-control" name="date_from" value="{{ date('Y-m-d') }}">
 
-                        <label class="mt-3">Creado desde <span class="text-muted">Opcional</span></label>
-                        <input type="date" class="form-control" name="date_created" value="{{ date('Y-m-d') }}">
+                    <label class="mt-3">Fecha Hasta<span class="text-muted">Opcional</span></label>
+                    <input type="date" class="form-control" name="date_until" value="{{ date('Y-m-d') }}">
 
-                        <label class="mt-3">Artista(s)</label>
-                        <select class="form-control" id="artists" name="artists[]" multiple>
-                            @foreach($artists as $artist)
-                                <option value="{{ $artist->id }}">{{ $artist->name }}</option>
-                            @endforeach
-                        </select>
+                    <label class="mt-3">Artista(s)</label>
+                    <select class="form-control" id="artists" name="artists[]" multiple>
+                        @foreach($artists as $artist)
+                            <option value="{{ $artist->id }}">{{ $artist->name }}</option>
+                        @endforeach
+                    </select>
 
-                        <button type="submit" class="btn btn-dark btn-block mt-3"><i class="fas fa-search"></i> Buscar</button>
-                    </form>
-                </div>
+                    <label class="mt-3">Servicio(s)</label>
+                    <select class="form-control" id="services" name="services[]" multiple>
+                        @foreach($services as $service)
+                            <option value="{{ $service->id }}">{{ $service->name }}</option>
+                        @endforeach
+                    </select>
+
+                    <label class="mt-3">Estado de Pago</label>
+                    <select class="form-control" id="payment_statuses" name="payment_statuses[]" multiple>
+                        @foreach($payment_statuses as $payment_status)
+                            <option value="{{ $payment_status['name'] }}">{{ $payment_status['label'] }}</option>
+                        @endforeach
+                    </select>
+
+                    <button type="submit" class="btn btn-success btn-block mt-3"><i class="fas fa-search"></i> Buscar</button>
+                </form>
             </div>
         </div>
     </div>
+</div>
 
 @endsection
